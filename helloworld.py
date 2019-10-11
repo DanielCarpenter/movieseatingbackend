e65aa15d867d3f1e829e6155e01c67c2b370c1e2
DanielCarpenter <dmc2012@gmail.com>
2019-10-11 16:55:38 -0500

    Database addition

--- helloworld.py
+++ helloworld.py
@@ -5,12 +5,15 @@ from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.update(dict(secret_key="wrong"))
-
+#db = SQLAlchemy(app)
 admin = Admin(app)
 
 @app.route('/')
 def home():
     return render_template('home.html', hello="Get off my lawn!")
+
+#class User(db.Model):
+ #   name = db.Column(db.String(20))
 
 
 
