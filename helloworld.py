0bb9e99c6c8220fd3ed3260d264b4fe0c54e0dd3
DanielCarpenter <dmc2012@gmail.com>
2019-10-11 11:42:28 -0500

    Added flask admin, sqlalchemy, started css styles
    Not sure how to set up  the external style sheet to effect home.html or
    layout for that matter.

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,17 @@
 from flask import (Flask, render_template)
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.update(dict(secret_key="wrong"))
 
+admin = Admin(app)
 
 @app.route('/')
 def home():
     return render_template('home.html', hello="Get off my lawn!")
+
 
 
 if __name__ == '__main__':
