74a8008885e0d5de1d79086cda0c03125d800a5b
Daniel Carpenter <d_c176@txstate.edu>
2019-12-04 04:27:04 -0600

    Token Refresh Implemented

--- modules/access/routes.py
+++ modules/access/routes.py
@@ -1,4 +1,5 @@
 from modules.access import access_api
-from .controllers import ApiLogin
+from .controllers import ApiLogin, TokenRefresh
 
 access_api.add_resource(ApiLogin, '/access')
+access_api.add_resource(TokenRefresh, '/access/refresh')
