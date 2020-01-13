5adb38d1f938c4d11c00e3b3b78c47fcdd447aa9
Daniel Carpenter <d_c176@txstate.edu>
2020-01-12 22:45:45 -0600

    registration route change

--- modules/accounts/routes.py
+++ modules/accounts/routes.py
@@ -1,6 +1,6 @@
 from modules.accounts import account_api
 from .controllers import ApiRegister, ApiLogin, TokenRefresh
 
-account_api.add_resource(ApiRegister, '/users')
+account_api.add_resource(ApiRegister, '/register')
 account_api.add_resource(ApiLogin, '/login')
 account_api.add_resource(TokenRefresh, '/refresh')
