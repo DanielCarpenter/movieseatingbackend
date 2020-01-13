ad05c5f7f426c54ea2696ded6ff5042bae8ab030
Daniel Carpenter <d_c176@txstate.edu>
2020-01-12 22:45:23 -0600

    working user registration

--- modules/accounts/controllers.py
+++ modules/accounts/controllers.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource, reqparse
 from flask import jsonify
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_refresh_token_required)
-from flask_security.utils import verify_password
+from flask_security.utils import verify_password, encrypt_password
 from modules import db
 import datetime
 from modules.accounts import User, Role
+
+
 
 
 #code from here: https://blog.tecladocode.com/jwt-authentication-and-token-refreshing-in-rest-apis/
@@ -71,18 +73,17 @@ class ApiRegister(Resource):
                         help="This field cannot be blank."
                         )
     def post(self):
-        data = self.parser.parse_args()
-        if(User.find_by_username(data['username'])):
-            return {"message": "User with {} username already exists".format(data['username'])}, 409
+        self.args = self.parser.parse_args()
+        if(User.find_by_username(self.args['username'])):
+            return {"message": "User with {} username already exists".format(self.args['username'])}, 409
         else:
             user = User(
-                id = User.nextId(),
                 first_name = self.args['firstName'],
                 last_name = self.args['lastName'],
                 email = self.args['username'],
-                password = self.args['password'],
-                confirmed_at = datetime.datetime()
-                #Role = 'customer'
+                password = encrypt_password(self.args['password']),
+                active = True,
+                confirmed_at = datetime.datetime.now()
             )
             try:
                 db.session.add(user)
